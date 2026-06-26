547fd105268415585cab76f7fb262d491c18e0b2
1aman1 <amangupta@outlook.com>
2026-06-26 22:29:29 +0530

    2

---
+++ code-road/algorithm-paradigms/Recursion/playlist1/L18/kth-permutations.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string getKthPerm(int k, int N)
+{
+    k--;
+
+    int N = 10;
+    vector<int> numbers;
+
+    int fact = 1;
+    for (int i = 1; i < N; ++i)
+    {
+        fact *= i;
+        numbers.push_back(i);
+    }
+
+    numbers.push_back(N);
+    string ans = "";
+
+    while (true)
+    {
+        ans += to_string(numbers[k / fact]);
+        numbers.erase(numbers.begin() + (k / fact));
+
+        if (numbers.size() == 0)
+            break;
+
+        k %= fact;
+        fact = fact / numbers.size();
+    }
+    return ans;
+}
+
+int main()
+{
+    int N = 24;
+    int K = 17;
+
+    cout << getKthPerm(K, N);
+
+    return 0;
+}
